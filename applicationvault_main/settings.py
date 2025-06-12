e5185f7347311c233f41966c123a0ddc9a62e942
Dikshakapse <dikshakapse333@gmail.com>
2025-06-12 23:35:22 +0530

    .

--- applicationvault_main/settings.py
+++ applicationvault_main/settings.py
@@ -1,4 +1,5 @@
 from pathlib import Path
+from decouple import config
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -8,12 +9,13 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-3nzr7xq)d55u6@2&g2u+%@p)=h+x5dougrdo14g-nhf-t5#aej'
+
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+SECRET_KEY = config('SECRET_KEY')
+DEBUG = config('DEBUG', default=False, cast=bool)
+ALLOWED_HOSTS = config('ALLOWED_HOSTS', default='').split(',')
 
-ALLOWED_HOSTS = []
 
 
 # Application definition
